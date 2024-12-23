d5ec5acddad91d9629800c75034bfabf5333c47c
WarCrazhed <mariozamora5126@gmail.com>
2024-12-22 21:07:32 -0600

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Reply;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->create(['email' => 'mario@app.com']);
+        User::factory(9)->create();
+        Category::factory(10)
+            ->hasThreads(20)
+            ->create();
+        Reply::factory(400)->create();
     }
 }
